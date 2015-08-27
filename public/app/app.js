45680efeeb156714577b4964fdddf9ca18cea260
Tony <david.guerrero87@live.com>
2015-08-27 12:30:09 -0700

    Auth Service created

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,9 @@
-angular.module('userApp', ['ngAnimate', 'app.routes', 'mainCtrl', 'userCtrl', 'userService'])
+angular.module('userApp', ['ngAnimate', 'app.routes', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
 
 // application configuration to integrate token into requests
-.config(function() {
+.config(function($httpProvider) {
 
 	// attach our auth interceptor to the http requests
-
+	$httpProvider.interceptors.push('AuthInterceptor');
 
 });
